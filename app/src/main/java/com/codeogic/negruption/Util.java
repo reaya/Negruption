fd22a2dbeb111ccac635a7d309d287593b345298
reaya <reaya.github@gmail.com>
2017-05-11 15:47:50 +0530

    Initial commit

---
+++ app/src/main/java/com/codeogic/negruption/Util.java
@@ -0,0 +1,18 @@
+package com.codeogic.negruption;
+
+/**
+ * Created by win-7 on 5/3/2017.
+ */
+
+public class Util {
+
+    public static final String INSERT_USER = "http://codeogic.esy.es/negruption/insertuser.php";
+
+    public static final String LOGIN_USER = "http://codeogic.esy.es/negruption/negruptlogin.php";
+
+    public static final String PREFS_NAME="negruption";
+    public static final String PREFS_KEYUSERNAME="keyUsername";
+    public static final String PREFS_KEYPASSWORD="keyPassword";
+
+
+}
